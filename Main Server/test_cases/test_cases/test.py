0e083b5aed64efd31194ee64dfcce8b423cd91dd
harsha-ys <hyoshith@gmai.com>
2021-01-21 10:04:24 -0800

    user routes test cases added

---
+++ Main Server/test_cases/test_cases/test.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("app/101",qos=1)
+    
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client("101",False)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("3.89.109.97", 1883)
+client.loop_forever()
