c0b154ac62fec9e4f9214fa512f866b154cfa93e
supunvindula <supunvindula98@gmail.com>
2021-01-31 00:22:43 +0530

    added aws address

--- mobileApp/FishTankController/lib/constants.dart
+++ mobileApp/FishTankController/lib/constants.dart
@@ -1,10 +1,9 @@
 
-const String LOGINURL="http://192.168.43.68:33000/login";
-const String SIGNUPURL='http://192.168.43.68:33000/signup';
-const String GETTANKIDSURL='http://192.168.43.68:33000/app/getdevicedata';
-const String ADDTANKURL='http://192.168.43.68:33000/app/addtank';
-const String GETFISHNAMES='http://192.168.43.68:33000/app/fishnames';
-const String FEEDNOW="http://192.168.43.68:33000/control/feed";
-const String RENEW="http://192.168.43.68:33000/control/renew";
-const String GRAPHURL="http://192.168.43.68:33000/app/retrivedata";
-
+const String LOGINURL="http://35.173.230.92:33000/login";
+const String SIGNUPURL='http://35.173.230.92:33000/signup';
+const String GETTANKIDSURL='http://35.173.230.92:33000/app/getdevicedata';
+const String ADDTANKURL='http://35.173.230.92:33000/app/addtank';
+const String GETFISHNAMES='http://35.173.230.92:33000/app/fishnames';
+const String FEEDNOW="http://35.173.230.92:33000/control/feed";
+const String RENEW="http://35.173.230.92:33000/control/renew";
+const String GRAPHURL="http://35.173.230.92:33000/app/retrivedata";
